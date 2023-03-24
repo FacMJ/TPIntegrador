220d93dd81cd6582fb6702233ee0ba77fa92b0ef
FacMJ <facumarcialjais24@gmail.com>
2023-03-23 23:22:49 -0300

    Agregado: clases Equipo, Pronostico y Resultado

--- src/main/java/LectorCSV.java
+++ src/main/java/LectorCSV.java
@@ -1,12 +1,9 @@
 import java.io.IOException;
-import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
 
 /* 
  
