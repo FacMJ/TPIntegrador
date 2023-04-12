78b11b9f85884992d7af738b82ac86ee5a0ee182
FacMJ <116842080+FacMJ@users.noreply.github.com>
2023-04-12 19:27:05 -0300

    Create Persona.java

---
+++ src/main/java/Persona.java
@@ -0,0 +1,19 @@
+public class Persona {
+        private String Nombre;
+        private Pronóstico [] Pronósticos;
+        private int puntos;
+        int Suma = 0;
+            for (Pronóstico p: Pronósticos){
+
+    }
+
+    public Persona (String Nombre, Pronóstico [] Pronósticos, int puntos, Suma = suma + p.puntos()){
+        this.Nombre = Nombre;
+        this.Pronósticos = Pronósticos;
+        this.puntos = suma;
+
+
+    }
+
+
+}
